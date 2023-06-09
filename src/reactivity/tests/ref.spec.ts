2acd77697ffd7dc53d69c19fc418a2826dc42170
xun.hainan <xun.hainan@zol.com.cn>
2023-06-09 19:29:20 +0800

    proxyRefs

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from '../effect'
 import { reactive } from '../reactive'
-import { ref, isRef, unRef } from '../ref'
+import { ref, isRef, unRef, proxyRefs } from '../ref'
 
 describe('ref', () => {
   it('happy path', () => {
@@ -59,5 +59,25 @@ describe('ref', () => {
     expect(unRef(a)).toBe(1)
     expect(unRef(1)).toBe(1)
     // expect(isRef(user)).toBe(false)
+  })
+
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name:"xiaohong"
+    }
+
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(10)
+    expect(proxyUser.age).toBe(10)
+    expect(proxyUser.name).toBe("xiaohong")
+
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(30)
+    expect(proxyUser.age).toBe(30)
+    expect(user.age.value).toBe(30)
   })
 })
